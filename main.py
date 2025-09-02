901f82474ba4d5610a4f018d2d1a2de8875ab032
miguelmonterodev <miguelmonterodev@gmail.com>
2025-09-02 13:31:13 +0100

    handle IndexError when no argument as an input

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 # accept a command line argument for the prompt.
-if sys.argv[1]:
+try:
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print("Response tokens: ")#response.usage_metadata.candidates_token_count
-else:
-    print("Error: we need argument as an input")
+except IndexError:
+    print("Error: we need argument as an input. Example: uv run main.py 'input'")
+    
